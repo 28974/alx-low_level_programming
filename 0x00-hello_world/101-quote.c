079d8a50064d883cf87c44c4cce53fc7f47ebe96
28974 <germainetsakoue@yahoo.com>
2022-03-10 13:51:06 -0800

    dej

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,6 @@
  */
 int main(void)
 {
-	fprintf(stderr,
-"and that piece of art is useful\" - Dora Kopar, 2015-10-19/n");
+	write(2, "and that piece of art is useful\" - Dora Kopar, 2015-10-19/n", 58);
 	return (1);
 }
