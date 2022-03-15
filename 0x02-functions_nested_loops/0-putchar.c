6b7b4ae7045d624a88e9f98e313c010a1ba7b501
28974 <germainetsakoue@yahoo.com>
2022-03-15 12:52:02 -0700

    pluts

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,22 @@
 #include "main.h"
-
 /**
  * main - program that prints _putchar
  *
- * _putchar print the character given
- * Return: Always 0.
+ * Return: Always 0
  */
 
 int main(void)
 {
-	char y[] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
-	int h = 8;
-	while (y[h] != 0 '\0')
-	{
-	_putchar(y[h]);
-
-	h++;
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
+
