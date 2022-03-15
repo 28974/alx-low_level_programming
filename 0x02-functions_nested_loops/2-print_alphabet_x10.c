65eb9afdcdfb5c11621804af8e1d80d8aed94b22
28974 <germainetsakoue@yahoo.com>
2022-03-15 14:27:43 -0700

    times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,8 @@
 #include "main.h"
-
 /**
  * print_alphabet_x10.c - Prints 10 times the alphabets
  */
-void print_alphabet_x10.c(void)
+void print_alphabet_x10(void)
 {
 	int count = 0;
 	char letter;
