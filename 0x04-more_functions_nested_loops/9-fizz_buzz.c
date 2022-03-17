ba957849f191b0862416aa24a425424793028e3a
28974 <germainetsakoue@yahoo.com>
2022-03-17 14:50:28 -0700

    fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -4,26 +4,28 @@
  * main - program
  * But for multuples
  * and for the multiples
+ * when there are multiples
+ *
  * Return: 0
  */
 
 int main(void)
 {
-	int num;
+	int i;
 
-	for (num = 1; num <= 100; num++)
+	for (i = 1; i <= 100; i++)
 	{
-		if (num % 3 == 0 && num % 5 == 0)
-			printf("FizzBuzz ");
-		else if (num % 3 ==0 && num % 5 != 0)
-			printf("Fizz ");
-		else if (num == 100)
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
 			printf("Buzz");
-		else if (num % 5 == 0 && num % 3 != 0)
-			printf("Buzz ");
 		else
-			printf("%d ", num);
+			printf("%d", i);
+		if (i != 100)
+			putchar(' ');
 	}
-	printf("\n");
+	putchar('\n');
 	return (0);
 }
