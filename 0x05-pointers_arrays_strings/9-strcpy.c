9e7158ceab7fe3a5c61e99df5e29d43728581a0e
28974 <germainetsakoue@yahoo.com>
2022-03-21 09:15:13 -0700

    copies_the_string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * *_strcpy - function that copy the string
+ * @src: pointer to source char
+ * Return: char
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		*(dest + 1) = *(src + 1);
+	}
+	*(dest + 1) = '\0';
+	return(dest);
+}
