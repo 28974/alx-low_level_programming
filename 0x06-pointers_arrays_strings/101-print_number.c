2935c370ecfa1b8a236727fff40602817ccc4d2f
28974 <germainetsakoue@yahoo.com>
2022-03-24 11:37:15 -0700

    print

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,16 +8,28 @@
 
 void print_number(int n)
 {
-	unsigned int num = n;
+	int nani, tmpoi, exposant;
 
-	if (n < 0)
+	exposant = 1;
+	/*check negatives*/
+	if (n >= 0)
+		nani = n * -1;
+	else
 	{
+		nani = n;
 		_putchar('-');
-		num = -num;
 	}
-
-	 if ((num / 10) > 0)
-		 print_number(num / 10);
-
-	 _putchar((num % 10) + '0');
+	/*Initialize exposant variable*/
+	tmpoi = nani;
+	while (tmpoi <= -10)
+	{
+		exposant *= 10;
+		tmpoi /= 10;
+	}
+	/*Main*/
+	while (exposant >= 1)
+	{
+		_putchar (((nani / exposant) % 10) * -1 + '0');
+		exposant /= 10;
+	}
 }
