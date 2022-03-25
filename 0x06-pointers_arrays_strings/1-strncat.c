4c735b8ed5c2c23fd99bfb56f6bbb547ec6b7011
28974 <germainetsakoue@yahoo.com>
2022-03-25 00:01:24 -0700

    concatenated

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,14 +10,15 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int index = 0, dest_len = 0;
+	int i, k;
 
-	while (dest[index++])
-		dest_len++;
-
-	for (index = 0; src[index] && index < n; index++)
-		dest[dest_len++] = src[index];
-
+	i = 0;
+	for (k = 0; dest[k] != '\0'; k++)
+		;
+	while (src[i] != '\0' && i < n)
+	{
+		dest[k + i] = src[i];
+		i++;
+	}
 	return (dest);
 }
-
