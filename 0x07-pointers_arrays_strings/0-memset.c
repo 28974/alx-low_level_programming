7865e9d9df6ce2a8b653ad4ec0af125532b341d2
28974 <germainetsakoue@yahoo.com>
2022-03-28 10:25:15 -0700

    fills

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,13 +11,9 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i = 0;
+	unsigned int i;
 
-	while (i < n)
-	{
-
-		*(s + i) = b;
-		i += 1;
-	}
+	for (i = 0; i < n; i++)
+		s[i] = b;
 	return (s);
 }
