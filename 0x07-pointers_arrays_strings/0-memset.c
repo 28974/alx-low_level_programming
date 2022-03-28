3e7c47c5de9eb91fde82bae83dc8273ff577e982
28974 <germainetsakoue@yahoo.com>
2022-03-28 10:04:20 -0700

    fills_memory

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _memste - function that fills memory
+ *
+ * @s: memory area to return
+ * @b: constant style
+ * @n: size of bytes
+ * Return: char
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+
+		*(s + i) = b;
+		i += 1;
+	}
+	return (s);
+}
+
