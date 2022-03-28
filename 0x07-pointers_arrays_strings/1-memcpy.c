3b127161bcf72faa0e3dfbd3126b39b7dca3ef6f
28974 <germainetsakoue@yahoo.com>
2022-03-28 10:36:10 -0700

    fills

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,14 +11,10 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i = 0;
-	unsigned int j = 0;
+	unsigned int i;
 
-	while (i < n)
-	{
-		*(dest + i) = *(src + j);
-		i += 1;
-		j += 1;
-	}
+	for (i = 0; i < n; n++)
+		dest[i] = src[i];
+
 	return (dest);
 }
