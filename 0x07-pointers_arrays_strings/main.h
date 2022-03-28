beb97f97a6a0c4fb4e9de88815eea1d338a37a0f
28974 <germainetsakoue@yahoo.com>
2022-03-28 11:35:37 -0700

    main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 int _putchar(int c);
-char *_memset(char *s, char b, insigned int n);
+char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
-char _*strchr(char *s, char c);
+char *_strchr(char *s, char c);
 unsigned int _strstn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
