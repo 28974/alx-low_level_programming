1401c5c3b5f7c31210cf89ea0c58d8da25bed095
28974 <germainetsakoue@yahoo.com>
2022-03-30 11:32:00 -0700

    square root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - function that returns the square
+ * square root of number
+ * @n: input number
+ *
+ * Return: int
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+		return (0);
+	if (n == 1)
+		return (1);
+	if (n < 0)
+		return (-1);
+	return (getNumSqr(n, 1));
+}
+
+/**
+ * getNumSqr - function that get the num of sqroot
+ *
+ * @num: inpit number
+ * @i: counter variable
+ * Return: int
+ */
+
+int getNumSqr(int num, int i)
+{
+	if (num == (i * i))
+		return (i);
+	else if (num > (i * i))
+		return (getNumSqr(num, i + 1));
+	else
+		return (-1);
+}
