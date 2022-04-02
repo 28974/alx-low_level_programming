439f083eae1e5258cf04f66584eb7c01e648a6b9
28974 <germainetsakoue@yahoo.com>
2022-04-02 10:27:03 -0700

    change

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * is_valid_int - checks if a string is a valid integer
+ * @s: string
+ *
+ * Return: 1 is s is a valid integer. 0 otherwise
+ */
+int is_valid_int(char *s)
+{
+	while (*s)
+	{
+		if (*s < '0' || *s > '9')
+			return (0);
+
+		s++;
+	}
+	return (1);
+}
+/**
+ * main - output minimum number of coins
+ * changing for an amount of cents.
+ * @argc: count's argument
+ * @argv: vector's argument
+ * Return: 0 if success
+ */
+int main(int argc, char *argv[])
+{
+	int cents, coins = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	cents = atoi(argv[1]);
+
+	while (cents > 0)
+	{
+		coins++;
+		if ((cents - 25) >= 0)
+		{
+			cents -= 25;
+			continue;
+		}
+		if ((cents - 10) >= 0)
+		{
+			cents -= 10;
+			continue;
+		}
+		if ((cents - 5) >= 0)
+		{
+			cents -= 5;
+			continue;
+		}
+		if ((cents - 2) >= 0)
+		{
+			cents -= 2;
+			continue;
+		}
+		cents--;
+	}
+
+	printf("%d\n", coins);
+	return (0);
+}
