1c57ac3222bfd6669bbe2c747f24496580fcf014
28974 <germainetsakoue@yahoo.com>
2022-04-02 09:56:13 -0700

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -6,4 +6,53 @@
  * is_valid_int - checks if a string is a valid integer
  * @s: string
  *
- *
+ * Return: 1 is s a valid integer. 0 otherwise
+ */
+
+int is_valid_int(char *s)
+{
+	while (*s)
+	{
+		if (*s < '0' || *s > '9')
+			return (0);
+
+		s++;
+	}
+
+	return (1);
+}
+/**
+ * main - adding all arguments together if they are digits.
+ * @argc: argument count only accepts ants separated by spaces.
+ * @argv: argument vector
+ * Return: 0 on success, 1 on failure
+ */
+int main(int argc, char **argv)
+{
+	int i;
+	int total = 0;
+
+	/* if no other arguments are passed */
+	if (argc < 2)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		/* check if the argument is a valid integer */
+		if (is_valid_int(argv[i]))
+		{
+			total += atoi(argv[i]);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", total);
+
+	return (0);
+}
