07573546a282f312e06ffb67722aae534e357022
28974 <germainetsakoue@yahoo.com>
2022-04-04 21:35:08 +0100

    free a 2 dimensional grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+  * free_grid - use free function on previous grid
+  * @grid: 2 dimension array to free
+  * @height: input heigth
+  *
+  * Return: free 'd grid
+  */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+
+	free(grid);
+}
