5da3c4eedb2d9c2630987e4edbd759725b68b944
28974 <germainetsakoue@yahoo.com>
2022-04-06 20:12:55 +0100

    add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,40 +1,34 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
- * infinite_add - Adds two numbers
- * @n1: first number
- * @n2: second number
- * @r: the buffer
- * @size_r: buffer size
- *
- * Return: if r can store
- *         if r cannot store
+ * infinite_add - summing numbers
+ * @n1: first string
+ * @n2: second  string
+ * @r: pointer to buffer where result is stored
+ * @size_r: requested size for the buffer
+ * Return: pointer to buffer where result is stored
  */
 
-char *infinite_add(char *n1, char *n2, char *r, char size_r)
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int i, j, n;
-
-	i = j = n =0;
-
+	
+	i = j = n = 0;
 	if ((n1[0] - '0') + (n2[0] - '0') >= 10)
 	{
 		r[0] = 1 + '0';
 		j = 1;
 	}
-
-	while (i < size_r && (n1[i] != '\0' || n2[i] != '\0' || r[j] '\0'))
+	while (i < size_r && (n1[i] != '\0' || n2[i] != '\0' || r[j] != '\0'))
 	{
 		if ((n1[i + 1] - '0') + (n2[i + 1] - '0') >= 10)
-           		n = 1;
-
+			n = 1;
 		else
 			n = 0;
-		r[j] = (n[i] - '0') + (n2[i] - '0') + n;
+		r[j] = (n1[i] - '0') + (n2[i] - '0') + n;
 		r[j] = r[j] % 10 + '0';
 		i++;
 		j++;
-
 		if (n1[i] == '\0' || n2[i] == '\0')
 			r[j] = '\0';
 	}
