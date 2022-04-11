79e038d6567308b66caaa746c0c5b15d73f9749a
28974 <germainetsakoue@yahoo.com>
2022-04-11 19:49:32 +0100

    main

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,9 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include "holberton.h"
 /**
- * main - Entry Point
- * Return: 0
- */
+  * main - printing our file when it is compiled from
+  * @void: nothing
+  * Return: 0 on success
+  */
 int main(void)
 {
 	printf("%s\n", __FILE__);
